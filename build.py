6b5b0051b81444c0ef7ba200cbae2aa836ded2bc
sagarambalam <sagar.ambalam@gmail.com>
2017-06-23 09:43:47 +0000

    Done

--- build.py
+++ build.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 def solution(t):
-    list_a =[]
-    for i in range(len(t)):
-        list_a.append(t[i])
-    return list_a
+    return list(t)
+
+
+    #list_a =[]
+    #for i in range(len(t)):
+    #    list_a.append(t[i])
+    #return list_a
